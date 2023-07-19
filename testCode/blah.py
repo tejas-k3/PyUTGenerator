67961fd75ee2175c5d149b64ed8d5b859d6bb94d
Tejas <tejaskothari03@gmail.com>
2023-07-19 23:35:59 +0530

    Test file

---
+++ testCode/blah.py
@@ -0,0 +1,11 @@
+class TestCoder:
+    def printA(z):
+        print("rpintA ",z)
+    def printB(z):
+        print("printB ",z)
+
+class TestCode2:
+    def printTCode(z):
+        print("printTCode ",z)
+    def printTTT():
+        print("printer under Code testcode")
